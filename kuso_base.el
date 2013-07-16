150ef97b3e98f433e02a96453a5a8282db51d6e6
Sameer Rahmani <lxsameer@gnu.org>
2013-07-17 02:52:44 +0430

    dpaste plugin added

--- kuso_base.el
+++ kuso_base.el
@@ -1,6 +1,22 @@
+;;   Kuso IDE
+;;    Copyright (C) 2010-2013  Sameer Rahmani <lxsameer@gnu.org>
+;;
+;;    This program is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    any later version.
+;;
+;;    This program is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+;;
+;;    You should have received a copy of the GNU General Public License
+;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Autocomplete configurations
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.kuso.d/ac-dict")
+(add-to-list 'ac-dictionary-directories (concat default-directory "../ac-dict"))
 (ac-config-default)
 
 (global-auto-complete-mode t)
@@ -53,3 +69,8 @@
 (load-library "hideshow")
 (global-set-key (kbd "C-\-") 'hs-toggle-hiding)
 (global-set-key (kbd "C-\\") 'toggle-selective-display)
+
+;; Load abou submenu
+(load-file (concat default-directory "kuso_version.el"))
+(load-file (concat default-directory "kuso_about.el"))
+(load-file (concat default-directory "kuso_dpaste.el"))
