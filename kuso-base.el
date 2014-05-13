19975561343db1f95496490661a166d536caff2a
Keyvan Hedayati <k1.hedayati93@gmail.com>
2014-05-13 22:44:08 +0430

    Duplicate Start Of Line Or Region

--- kuso-base.el
+++ kuso-base.el
@@ -192,9 +192,36 @@
            (list (region-beginning) (region-end))
          (list (line-beginning-position) (line-beginning-position 2)))))
 
+
+;; http://www.emacswiki.org/emacs/DuplicateStartOfLineOrRegion
+(defun duplicate-start-of-line-or-region ()
+  (interactive)
+  (if mark-active
+      (duplicate-region)
+    (duplicate-start-of-line)))
+
+(defun duplicate-start-of-line ()
+  (let ((text (buffer-substring (point)
+                                (beginning-of-thing 'line))))
+    (forward-line)
+    (push-mark)
+    (insert text)
+    (open-line 1)))
+
+(defun duplicate-region ()
+  (let* ((end (region-end))
+         (text (buffer-substring (region-beginning)
+                                 end)))
+    (goto-char end)
+    (insert text)
+    (push-mark end)
+    (setq deactivate-mark nil)
+    (exchange-point-and-mark)))
+
 (global-set-key (kbd "M-;") 'comment-dwim-line)
 (global-set-key (kbd "C-x C-e") 'rename-current-buffer-file)
 (global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
 (global-set-key (kbd "C-w") 'kill-region)
+(global-set-key (kbd "M-S-<down>") 'duplicate-start-of-line-or-region)
 
 (provide 'kuso-base)
